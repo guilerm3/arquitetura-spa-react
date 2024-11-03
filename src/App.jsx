70bff41353bce5edbf202408ecb4facd0f9e8902
Guilherme Lucas <127703012+guilerm3@users.noreply.github.com>
2024-11-03 17:09:02 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,14 @@ import About from './pages/About'
 import Projects from './pages/Projects'
 import Contact from './pages/Contact'
 
+//UTILS
+import ScrollToTop from './utils/ScrollTop'
+
 function App() {
 
   return (
     <Router>
+      <ScrollToTop/>
       <Routes>
         <Route path='/' element={<Home/>}></Route>
         <Route path='/about' element={<About/>}></Route>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,39 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 //ASSETS
 import './Header.css'
 import Logo from '../../assets/dnc-logo.svg'
 
-function Header (){
+//COMPONENTS
+import Button from '../Button/Button'
+
+function Header() {
+    const [isOpen, setIsOpen] =useState(false)
+    const toggleMenu = () =>{
+        setIsOpen(!isOpen)
+    }
     return (
         <header>
             <div className="container">
                 <div className="al-center d-flex jc-space-between">
-                    <Link to='/'><img src={Logo}/></Link>
-                    <nav>
-                    <ul className='d-flex'>
-                        <li><Link to='/'>Home</Link></li>
-                        <li><Link to='/about'>About</Link></li>
-                        <li><Link to='/projects'>Projects</Link></li>
-                        <li><Link to='/contact'>Contact</Link></li>
-                    </ul>
-                </nav>
+                    <Link to='/'><img src={Logo} /></Link>
+                    <div className='mobile-menu'>
+                        <Button buttonStyle='secondary' onClick={toggleMenu}>
+                            Menu
+                        </Button>
+                    </div>
+                    <nav className={`${isOpen ? 'open' : ''}`}>
+                        <Button buttonStyle='unstyled' className='mobile-menu close-btn' onClick={toggleMenu}>
+                            X
+                        </Button>
+                        <ul className='d-flex'>
+                            <li><Link to='/'>Home</Link></li>
+                            <li><Link to='/about'>About</Link></li>
+                            <li><Link to='/projects'>Projects</Link></li>
+                            <li><Link to='/contact'>Contact</Link></li>
+                        </ul>
+                    </nav>
                 </div>
             </div>
         </header>
