60432d272a73f141130622eed15bb5157665aa78
Guilherme Lucas <127703012+guilerm3@users.noreply.github.com>
2024-11-18 12:37:06 -0300

    sessionStorage and localStorage
    LocalStorage para salvar a linguagem da página e SessionStorage para salvar 'like' em projetos favoritos

--- src/components/ProjectsList/ProjectsList.jsx
+++ src/components/ProjectsList/ProjectsList.jsx
@@ -4,14 +4,30 @@ import { AppContext } from '../../contexts/AppContext'
 
 /*ASSETS*/
 import LikeFilled from '../../assets/like-filled.svg'
-import Like from '../../assets/like.svg'
+import LikeOutline from '../../assets/like.svg'
+
+/*COMPONENTS*/
+import Button from '../Button/Button'
 
 /*UTILS*/
 import { getApiData } from '../../services/apiServices'
 
 function ProjectsList(props) {
+    const [projects, setProjects] = useState()
+    const [favProjects, setFavProject] = useState([])
     const appContext = useContext(AppContext)
-    const [projects, setProjects] = useState([])
+    const handleSavedProjects = (id) =>{
+        setFavProject((prevFavProjects) =>{
+            if(prevFavProjects.includes(id)){
+                const filterArray = prevFavProjects.filter((projectId) => projectId !== id)
+                sessionStorage.setItem('favProjects', JSON.stringify(filterArray))
+                return prevFavProjects.filter((projectId) => projectId !== id)
+            } else{
+                sessionStorage.setItem('favProjects',JSON.stringify([...prevFavProjects, id]))
+                return [...prevFavProjects, id]
+            }
+        })
+    }
 
     useEffect(() => {
         const fetchData = async () => {
@@ -25,6 +41,13 @@ function ProjectsList(props) {
 
         fetchData()
     }, [])
+
+    useEffect(() =>{
+        const savedFavProjects = JSON.parse(sessionStorage.getItem('favProjects'))
+        if(savedFavProjects){
+            setFavProject(savedFavProjects)
+        }
+    },[])
 
     return (
         <div className='projects-section'>
@@ -43,7 +66,9 @@ function ProjectsList(props) {
                                 ></div>
                                 <h3>{project.title}</h3>
                                 <p>{project.subtitle}</p>
-                                <img src={Like} height='20px' />
+                                <Button buttonStyle='unstyled' onClick={()=>handleSavedProjects(project.id)}>
+                                    <img src={favProjects.includes(project.id) ? LikeFilled: LikeOutline} height='20px' />
+                                </Button>
                             </div>
                         ))
                     : 
